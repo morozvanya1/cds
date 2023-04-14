3f7b9f87cfd0718954ba5620f94993b6b928616f
Ivan M <im@kalite.ro>
2023-04-14 23:07:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // var cors_api_url = "https://cors-anywhere.herokuapp.com/";
+  function doCORSRequest(options, printResult) {
+    var x = new XMLHttpRequest();
+    // x.open(options.method, cors_api_url + options.url);
+    x.open(options.method, options.url);
+    x.onload = x.onerror = function () {
+      printResult(x.responseText);
+    };
+    x.send(options.data);
+  }
+
+  function getTransport(e) {
+    var urlField = document.getElementById("url");
+    var outputField = document.getElementById("output");
+    e.preventDefault();
+    doCORSRequest(
+      {
+        method: "GET",
+        url: urlField.value,
+      },
+      function printResult(result) {
+        var from = result.search("busstop");
+        var to = result.length;
+        var newstr = result.substring(from, to);
+        newstr = newstr.split('<div class="footer">')[0];
+        newstr = newstr.split('</a><br><a href=prediction.php?');
+        newstr = newstr.join();
+        newstr = newstr.split('</a>')[0];
+        newstr = newstr.split(',busstop=');
+        newstr = newstr.join();
+        newstr = newstr.split('busstop=')[1];
+        newstr = newstr.replaceAll(">", ' ');
+        newstr = newstr.split(',');
+
+        outputField.value = newstr;
+        console.log(newstr)
+      }
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Проверка ответа от сервера</h1>
+      <label>
+        Введите URL:{" "}
+        <input
+          type="url"
+          id="url"
+          defaultValue="https://m.cdsvyatka.com/marsh_stops.php?marshlist=1001"
+          size={500}
+        />
+      </label>
+      <label>
+        <button id="get" onClick={(e) => getTransport(e)}>
+          GET
+        </button>
+      </label>
+      <div id="bottom">
+        <textarea id="output"></textarea>
+      </div>
     </div>
   );
 }
