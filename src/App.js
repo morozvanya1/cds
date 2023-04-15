5cf4149926283a4975c7a167530573726f5af28a
Ivan M <im@kalite.ro>
2023-04-15 09:37:30 +0300

    add dark

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React from "react";
+import {default as logo} from "./front-of-bus.png";
+import {default as sun} from "./sun-bright.png";
+import {default as dark} from "./moon.png";
 
 function App() {
   const autoVal = ["1001", "1002", "1003", "1005", "1008", "1009", "1010", "1011", "1012", "1014", "1015", "1016", "1019", "1020", "1021", "1022", "1023", "1026", "1033", "1038", "1039", "1044", "1046", "1047", "1050", "1051", "1052", "1053", "1054", "1061", "1067", "1070", "1074", "1084", "1087", "1090", "2046", "2053", "3101", "3104", "3106", "3107", "3108", "3109", "3112", "3115", "3116", "3117", "3121", "3125", "3127", "3129", "3130", "3133", "3136", "3138", "3143", "3146", "3147", "3148", "3154", "3155", "3158", "3160", "3168", "50010", "50030", "50040", "50050", "50070", "50080"];
@@ -118,13 +121,40 @@ function App() {
     );
   }
 
+  function changeTheme() {
+    const v = document.getElementById("darkTheme").src;
+    if (v === sun) {
+      document.getElementById("darkTheme").src = dark;
+      document.querySelector(".App").style.background = "white";
+      document.querySelector(".darkTheme1").style.color = "rgb(26, 31, 37)";
+      document.querySelector(".content").style.borderTop = "1px solid rgba(0, 0, 0, 0.1)";
+      document.querySelector(".darkTheme2").style.color = "rgb(26, 31, 37)";
+      document.querySelectorAll(".darkTheme3").forEach(function(elem){
+        elem.style.color = "rgb(26, 31, 37)";
+      })
+    } else {
+      document.getElementById("darkTheme").src = sun;
+      document.querySelector(".App").style.background = "rgb(26, 31, 37)";
+      document.querySelector(".darkTheme1").style.color = "rgba(255, 255, 255, 0.75)";
+      document.querySelector(".content").style.borderTop = "1px solid rgba(255, 255, 255, 0.1)";
+      document.querySelector(".darkTheme2").style.color = "rgba(255, 255, 255, 1)";
+      document.querySelectorAll('.darkTheme3').forEach(function(elem){
+        elem.style.color = "rgba(255, 255, 255, 0.75)";
+      })
+    }
+    console.log(v);
+    console.log(sun);
+  }
+
   return (
     <div className="App">
       <header>
+        <img src={logo} alt="logo"/>
         <h1>ЦДС Вятка</h1>
+        <img src={sun} alt="logo" id="darkTheme" onClick={()=>changeTheme()}/>
       </header>
       <div className="content-header">
-        <p>Поиск по маршруту</p>
+        <p className="darkTheme1">Поиск по маршруту</p>
         <select name="marshlist" id="marshlist">
           {autoDesc.map((v, i) => (
             <option key={i} value={autoVal[i]}>
@@ -137,21 +167,21 @@ function App() {
         </button>
       </div>
       <div className="content">
-        <h3>{optVal}</h3>
+        <h3 className="darkTheme2">{optVal}</h3>
         <ul>
         {it && marshRes.map((v,i) => (
-          <li key={i} value={v.slice(0, v.indexOf(' '))} onClick={(e) => getInfo(e, v.slice(0, v.indexOf(' ')), v.slice(v.indexOf(' '), v.length))}>{v.slice(v.indexOf(' '), v.length)}</li>
+          <li className="darkTheme3" key={i} value={v.slice(0, v.indexOf(' '))} onClick={(e) => getInfo(e, v.slice(0, v.indexOf(' ')), v.slice(v.indexOf(' '), v.length))}>{v.slice(v.indexOf(' '), v.length)}</li>
         ))}
         {it4 && marshBack.map((v,i) => (
-          <li key={i} value={v.slice(0, v.indexOf(' '))} onClick={(e) => getInfo(e, v.slice(0, v.indexOf(' ')), v.slice(v.indexOf(' '), v.length))}>{v.slice(v.indexOf(' '), v.length)}</li>
+          <li className="darkTheme3" key={i} value={v.slice(0, v.indexOf(' '))} onClick={(e) => getInfo(e, v.slice(0, v.indexOf(' ')), v.slice(v.indexOf(' '), v.length))}>{v.slice(v.indexOf(' '), v.length)}</li>
         ))}
         <table>
         {it2 && (<tr className="table"><th>Маршрут</th><th>Ожидание</th><th>Следующая</th></tr>)}
         {it2 && marshRes.map((v,i) => (
           <tr key={i}>
-            <td>{v[0]}</td>
-            <td>{v[1]}</td>
-            <td>{v[2]}</td>
+            <td className="darkTheme3">{v[0]}</td>
+            <td className="darkTheme3">{v[1]}</td>
+            <td className="darkTheme3">{v[2]}</td>
           </tr>
         ))}
         </table>
