3b3b5ff8117534ce52d37df0482b1320c31b4229
Ivan M <im@kalite.ro>
2023-04-15 08:53:26 +0300

    add routes 4

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ function App() {
   const autoVal = ["1001", "1002", "1003", "1005", "1008", "1009", "1010", "1011", "1012", "1014", "1015", "1016", "1019", "1020", "1021", "1022", "1023", "1026", "1033", "1038", "1039", "1044", "1046", "1047", "1050", "1051", "1052", "1053", "1054", "1061", "1067", "1070", "1074", "1084", "1087", "1090", "2046", "2053", "3101", "3104", "3106", "3107", "3108", "3109", "3112", "3115", "3116", "3117", "3121", "3125", "3127", "3129", "3130", "3133", "3136", "3138", "3143", "3146", "3147", "3148", "3154", "3155", "3158", "3160", "3168", "50010", "50030", "50040", "50050", "50070", "50080"];
   const autoDesc = ["Авт 1", "Авт 2", "Авт 3", "Авт 5", "Авт 8", "Авт 9", "Авт 10", "Авт 11", "Авт 12", "Авт 14", "Авт 15", "Авт 16", "Авт 19", "Авт 20", "Авт 21", "Авт 22", "Авт 23", "Авт 26", "Авт 33", "Авт 38", "Авт 39", "Авт 44", "Авт 46", "Авт 47", "Авт 50", "Авт 51", "Авт 52", "Авт 53", "Авт 54", "Авт 61", "Авт 67", "Авт 70", "Авт 74", "Авт 84", "Авт 87", "Авт 90", "Авт К46", "Авт К53", "Авт 101", "Авт 104", "Авт 106", "Авт 107", "Авт 108", "Авт 109", "Авт 112", "Авт 115", "Авт 116", "Авт 117", "Авт 121", "Авт 125", "Авт 127", "Авт 129", "Авт 130", "Авт 133", "Авт 136", "Авт 138", "Авт 143", "Авт 146", "Авт 147", "Авт 148", "Авт 154", "Авт 155", "Авт 158", "Авт 160", "Авт 168", "Тролл Т1", "Тролл Т3", "Тролл Т4", "Тролл Т5", "Тролл Т7", "Тролл Т8"];
   const [marshRes, setMarshRes] = React.useState([]);
+  const [marshBack, setMarshBack] = React.useState([]);
   const [optVal, setOptVal] = React.useState("");
   var cors_api_url = "https://cors-anywhere.herokuapp.com/";
   var mVal = 0, mHtml = "";
   const [it, setIt] = React.useState(false);
   const [it2, setIt2] = React.useState(false);
+  const [it3, setIt3] = React.useState(false);
+  const [it4, setIt4] = React.useState(false);
   const [urlVal, setUrlVal] = React.useState("");
 
   function onSetMarsh() {
@@ -51,8 +54,11 @@ function App() {
         newstr = newstr.replaceAll(">", " ");
         newstr = newstr.split(",");
         setMarshRes(newstr);
+        setMarshBack(newstr)
         setIt(true); 
         setIt2(false);
+        setIt3(false);
+        setIt4(false);
         console.log(marshRes);
       }
     );
@@ -105,6 +111,8 @@ function App() {
         setMarshRes(newstr);
         setIt(false);
         setIt2(true);
+        setIt3(true);
+        setIt4(false);
         console.log(newstr);
       }
     );
@@ -134,6 +142,9 @@ function App() {
         {it && marshRes.map((v,i) => (
           <li key={i} value={v.slice(0, v.indexOf(' '))} onClick={(e) => getInfo(e, v.slice(0, v.indexOf(' ')), v.slice(v.indexOf(' '), v.length))}>{v.slice(v.indexOf(' '), v.length)}</li>
         ))}
+        {it4 && marshBack.map((v,i) => (
+          <li key={i} value={v.slice(0, v.indexOf(' '))} onClick={(e) => getInfo(e, v.slice(0, v.indexOf(' ')), v.slice(v.indexOf(' '), v.length))}>{v.slice(v.indexOf(' '), v.length)}</li>
+        ))}
         <table>
         {it2 && (<tr className="table"><th>Маршрут</th><th>Ожидание</th><th>Следующая</th></tr>)}
         {it2 && marshRes.map((v,i) => (
@@ -146,6 +157,7 @@ function App() {
         </table>
         </ul>
         {it2 && (<button onClick={(e) => getInfo(e, "", "", urlVal, optVal)}>Обновить</button>)}
+        {it3 && (<button onClick={() => setIt4(true)}>Назад</button>)}
       </div>
     </div>
   );
